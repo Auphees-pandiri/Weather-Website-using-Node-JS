c94f96b37610a7c05696a7eb3e8ac371bf2c2fab
aphees123 <apheespandiri123@gmail.com>
2019-12-21 15:34:25 +0530

    Set Up for Heroku

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname,'../public')))
 
 app.get('',(req,res)=>{
     res.render('index',{
-        title:'Weather Forecating App',
+        title:'Weather App',
         name:'aphees'
     })
 })
